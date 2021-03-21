b5bf9f1bc918d4b6e5e3a15f81b9622296f6f8ce
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-03-21 16:49:48 -0400

    updates to the replay buffer

--- replay_buffer.py
+++ replay_buffer.py
@@ -1,13 +1,15 @@
 import numpy as np
 
 class ReplayBuffer:
-    def __init__(self, max_size, input_shape, n_actions, discrete=False):
-        self.mem_size = max_size
+    def __init__(self, mem_size, input_shape, n_actions, discrete=False):
+        self.mem_size = mem_size
         self.discrete = discrete
-        self.state_memory = np.zeros((self.mem_counter) + input_shape)
+        self.state_memory = np.zeros((self.mem_size,) + input_shape)
         self.new_state_memory = np.zeros_like(self.state_memory)
-        dtype = np.int8 if self.discrete else np.float32
-        self.action_memory = np.zeros((self.mem_size, n_actions), dtype=dtype)
+        if discrete:
+            self.action_memory = np.zeros(self.mem_size, dtype=np.int32)
+        else:
+            self.action_memory = np.zeros((self.mem_size, n_actions), dtype=np.float32)
         self.reward_memory = np.zeros(self.mem_size)
         self.terminal_memory = np.zeros(self.mem_size, dtype=np.int8)
         self.mem_counter = 0
@@ -18,12 +20,7 @@ class ReplayBuffer:
         self.new_state_memory[index] = next_state
         self.reward_memory[index] = reward
         self.terminal_memory[index] = 1 - int(done)
-        if self.discrete:
-            actions = np.zeros(self.action_memory.shape[1])
-            actions[action] = 1.0
-            self.action_memory[index] = actions
-        else:
-            self.action_memory[index] = action
+        self.action_memory[index] = action
         self.mem_counter += 1
 
     def sample_buffer(self, batch_size):
@@ -34,6 +31,6 @@ class ReplayBuffer:
         next_states = self.new_state_memory[batch]
         rewards = self.reward_memory[batch]
         actions = self.action_memory[batch]
-        terminal = self.terminal_memory[batch]
+        terminals = self.terminal_memory[batch]
 
-        return states, actions, rewards, next_states, terminal
+        return states, actions, rewards, next_states, terminals
