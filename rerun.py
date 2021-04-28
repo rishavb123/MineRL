4be8f350cf9bf886619ade440470883f29c7fd54
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-04-27 22:29:32 -0400

    added checks

--- rerun.py
+++ rerun.py
@@ -4,4 +4,6 @@ def get_last_stamp():
     return int(os.listdir("models")[-1].split("_")[-1][:-3])
 
 def get_starting_episode(agent):
+    assert len(agent.metrics["kills"]) == len(agent.metrics["times"])
+    assert len(agent.metrics["kills"]) == len(agent.metrics["cumulative_rewards"])
     return len(agent.metrics["kills"]) + 1
