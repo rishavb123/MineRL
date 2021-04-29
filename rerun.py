4e43552bb1862c7aebbbdb05fd28cedc85ed1966
Rishav Bhagat <rishavbhagat.cs@gmail.com>
2021-04-29 09:31:05 -0400

    added get_stamps method

--- rerun.py
+++ rerun.py
@@ -3,6 +3,9 @@ import os
 def get_last_stamp():
     return int(os.listdir("models")[-1].split("_")[-1][:-3])
 
+def get_stamps():
+    return [int(os.listdir("models")[i + 1].split("_")[-1][:-3]) for i in range(len(os.listdir("models")) - 1)]
+
 def get_starting_episode(agent):
     assert len(agent.metrics["kills"]) == len(agent.metrics["times"])
     assert len(agent.metrics["kills"]) == len(agent.metrics["cumulative_rewards"])
